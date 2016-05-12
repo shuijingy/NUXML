8b5e0c95461d52426f62482decdeb455b1bea7ff
shuijingy <shuijingy@21cn.com>
2016-05-12 21:59:19 +0800

    +[UP] Add NGUI related files; Clean useless files

---
+++ Assets/Scripts/View/TestView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using NUXML;
+
+public class TestView : View 
+{
+	#region Fields
+	#endregion
+
+	#region Private Methods
+	#endregion	
+
+	#region Constructor
+	#endregion
+
+	#region Methods
+
+	public void StartGame()
+	{
+		Debug.Log("Play Clicked");
+	}
+
+	#endregion			
+}
+
+
